4f0e1c19bcdee52d817edb52df65c4031270a748
NatoDev <irenofrancinato@gmail.com>
2026-04-02 19:16:15 -0300

    feat: adicionar método para salvar novos produtos em ProdutoController

---
+++ src/main/java/com/natodev/ecommerce/domain/product/presentation/controller/ProdutoController.java
@@ -0,0 +1,35 @@
+package com.natodev.ecommerce.domain.product.presentation.controller;
+
+import com.natodev.ecommerce.domain.product.application.ProdutoService;
+import com.natodev.ecommerce.domain.product.presentation.dto.request.ProdutoRequestDTO;
+import com.natodev.ecommerce.domain.product.presentation.dto.response.ProdutoResponseDTO;
+import com.natodev.ecommerce.shared.response.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/produtos")
+@Slf4j
+@RequiredArgsConstructor
+public class ProdutoController {
+    private final ProdutoService produtoService;
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<ProdutoResponseDTO>> salvarProduto(
+            @Valid @RequestBody ProdutoRequestDTO request) {
+
+        ProdutoResponseDTO response = produtoService.salvarProduto(request);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(ApiResponse.sucesso(response));
+    }
+
+
+}
