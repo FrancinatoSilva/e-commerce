af9759c770f343f76bbc88357e95c856e0aba47c
NatoDev <irenofrancinato@gmail.com>
2026-04-02 16:03:44 -0300

    feat: criar metodo de resposta sucesso na ApiResponse

--- src/main/java/com/natodev/ecommerce/shared/response/ApiResponse.java
+++ src/main/java/com/natodev/ecommerce/shared/response/ApiResponse.java
@@ -20,5 +20,18 @@ public class ApiResponse<T> {
     @Builder.Default
     private LocalDateTime timestamp = LocalDateTime.now();
 
+    public static <T> ApiResponse<T> successo(T data) {
+        ApiResponse<T> response = new ApiResponse<>();
+        response.setSuccess(true);
+        response.setMessage("Sucesso");
+        response.setData(data);
+        response.setTimestamp(LocalDateTime.now());
+
+        return response;
+
+    }
+
+
+
 
 }
